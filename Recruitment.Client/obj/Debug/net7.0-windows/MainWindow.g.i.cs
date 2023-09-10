17db4cfec6e243ce4f0c6406aba74cbeec6bd1f7
piotrmichaljurkowski <piotrmichaljurkowski@gmail.com>
2023-09-11 00:19:03 +0200

    Removed files before gitignore update

--- Recruitment.Client/obj/Debug/net7.0-windows/MainWindow.g.i.cs
+++ Recruitment.Client/obj/Debug/net7.0-windows/MainWindow.g.i.cs
@@ -92,7 +92,7 @@ namespace Recruitment.Client {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Recruitment.Client;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Recruitment.Client;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
